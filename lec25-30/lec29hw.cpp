5cc869cd7d3828752fdd2f24db5de063cf108311
Muskan Dawar <sanchitdawar34@gmail.com>
2022-11-02 20:42:28 +0530

    2.11.22 lec 29 jaggered array

---
+++ lec25-30/lec29hw.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  // jaggered array using dynamic memory allocation
+  //  2 Rows
+  int *jagged[2];
+
+  // Allocate memory for elements in row 0
+  jagged[0] = malloc(sizeof(int) * 1);
+
+  // Allocate memory for elements in row 1
+  jagged[1] = malloc(sizeof(int) * 3);
+
+  // Array to hold the size of each row
+  int Size[2] = {1, 3}, k = 0, number = 100;
+
+  // User enters the numbers
+  for (int i = 0; i < 2; i++)
+  {
+
+    int *p = jagged[i];
+
+    for (int j = 0; j < Size[k]; j++)
+    {
+      *p = number++;
+
+      // move the pointer
+      p++;
+    }
+    k++;
+  }
+
+  k = 0;
+
+  // Display elements in Jagged array
+  for (int i = 0; i < 2; i++)
+  {
+
+    int *p = jagged[i];
+    for (int j = 0; j < Size[k]; j++)
+    {
+
+      printf("%d ", *p);
+      // move the pointer to the next element
+      p++;
+    }
+    printf("\n");
+    k++;
+    // move the pointer to the next row
+    jagged[i]++;
+  }
+}
