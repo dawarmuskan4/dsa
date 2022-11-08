ee78daf16656509e4292f9ec41876fd318314310
Muskan Dawar <sanchitdawar34@gmail.com>
2022-11-08 23:09:16 +0530

    8.11.22 upto lec 39 completed

--- lec31-40/lec35_mergesort.cpp
+++ lec31-40/lec35_mergesort.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void merge(int *arr, int s, int e)
-{
-
+void merge(int *arr, int s, int e){
   int mid = (s + e) / 2;
 
   int len1 = mid - s + 1;
@@ -14,14 +12,12 @@ void merge(int *arr, int s, int e)
 
   // copy values
   int mainArrayIndex = s;
-  for (int i = 0; i < len1; i++)
-  {
+  for (int i = 0; i < len1; i++){
     first[i] = arr[mainArrayIndex++];
   }
 
   mainArrayIndex = mid + 1;
-  for (int i = 0; i < len2; i++)
-  {
+  for (int i = 0; i < len2; i++){
     second[i] = arr[mainArrayIndex++];
   }
 
@@ -30,25 +26,20 @@ void merge(int *arr, int s, int e)
   int index2 = 0;
   mainArrayIndex = s;
 
-  while (index1 < len1 && index2 < len2)
-  {
-    if (first[index1] < second[index2])
-    {
+  while (index1 < len1 && index2 < len2){
+    if (first[index1] < second[index2]){
       arr[mainArrayIndex++] = first[index1++];
     }
-    else
-    {
+    else{
       arr[mainArrayIndex++] = second[index2++];
     }
   }
 
-  while (index1 < len1)
-  {
+  while (index1 < len1){
     arr[mainArrayIndex++] = first[index1++];
   }
 
-  while (index2 < len2)
-  {
+  while (index2 < len2){
     arr[mainArrayIndex++] = second[index2++];
   }
 
@@ -56,12 +47,9 @@ void merge(int *arr, int s, int e)
   delete[] second;
 }
 
-void mergeSort(int *arr, int s, int e)
-{
-
+void mergeSort(int *arr, int s, int e){
   // base case
-  if (s >= e)
-  {
+  if (s >= e){
     return;
   }
 
@@ -77,16 +65,13 @@ void mergeSort(int *arr, int s, int e)
   merge(arr, s, e);
 }
 
-int main()
-{
-
+int main(){
   int arr[15] = {3, 7, 0, 1, 5, 8, 3, 2, 34, 66, 87, 23, 12, 12, 12};
   int n = 15;
 
   mergeSort(arr, 0, n - 1);
 
-  for (int i = 0; i < n; i++)
-  {
+  for (int i = 0; i < n; i++){
     cout << arr[i] << " ";
   }
   cout << endl;
